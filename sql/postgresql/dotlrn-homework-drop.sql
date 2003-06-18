b3d993b29daca31c39896344bf5becf3d8ef7eb7
Don Baccus <dhogaza@pacifier.com>
2003-06-18 21:45:55 +0000

    Removed unnecessary SQL files (all done with Tcl callbacks using the newish service contract, notifications, and portal API)

--- sql/postgresql/dotlrn-homework-drop.sql
+++
@@ -1,8 +0,0 @@
-\i dotlrn-homework-applet-drop.sql
-\i dotlrn-homework-portlet-drop.sql
-
-select  content_type__unregister_relation_type (
-    'content_item',
-    'content_item',
-    'homework_correction'
-);
